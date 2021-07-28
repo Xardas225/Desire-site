9b05188559ac9da6230386b8aa56a424b40acc84
Alexandr <alexanderderevkov@yandex.ru>
2021-07-28 20:53:51 +0300

    added slider , rightside menu, half of collection

--- gulpfile.js
+++ gulpfile.js
@@ -51,6 +51,8 @@ function build() {
 
 function scripts() {
     return src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
